8f13076e76cb8d19ca7ad3e028bea569b9f289a3
tpurba <tpurba@byu.edu>
2024-02-12 08:36:28 -0700

    Finished finding upper and lower tangent

--- convex_hull.py
+++ convex_hull.py
@@ -35,7 +35,7 @@ class ConvexHullSolver(QObject):
 # to be displayed.
 
 	def showTangent(self, line, color):
-		self.view.addLines(line,color)
+		self.view.addLines(line, color)
 		if self.pause:
 			time.sleep(PAUSE)
 
@@ -56,56 +56,155 @@ class ConvexHullSolver(QObject):
 
 	def showText(self,text):
 		self.view.displayStatusText(text)
-
-
+	def sortClockWiseFromLeftmost(self, points, leftmost):
+				print("in sort clockwise\n")
+				def calculate_slope(point):
+					if point.x() == leftmost.x():
+						return float('-inf')
+					return (point.y() - leftmost.y()) / (point.x() - leftmost.x())
+				return sorted(points, key=calculate_slope)
+	def sortCounterClockWiseFromRightmost(self, points, rightmost):
+			def calculate_slope(point):
+				if point.x() == rightmost.x():
+					return float('inf')
+				return (point.y() - rightmost.y()) / (point.x() - rightmost.x())
+			return sorted(points, key=calculate_slope, reverse=True)
 
 	def mergeHulls(self, leftHull, rightHull):
 		print("left hull points: ", leftHull)
 		print("\nright hull points: ", rightHull)
-		copyCurrentPointsRight = rightHull.copy()
-		#sort by slope
-		def SortbySlopeClockWise(point): # in constant time sorts the points by slope in clock wise 
-				if (point.x() == copyCurrentPointsRight[len(rightHull)-1].x()):
-					slope = float('-inf')
-				else: 
-					slope = (point.y() - copyCurrentPointsRight[len(rightHull)-1].y()) / (point.x() - 
-										    copyCurrentPointsRight[len(rightHull)-1].x())
-				return slope
-		rightHull.sort(key=SortbySlope)
-		copyCurrentPointsLeft = leftHull.copy()
-		def sortBySlopeCounterClockWise(point): # Also just constant time
-				if (point.x() == copyCurrentPointsLeft[0].x()):
-					slope = float('inf')
-				else: 
-					slope = (point.y() - copyCurrentPointsLeft[0].y()) / (point.x() - copyCurrentPointsLeft[0].x())
-				return -1*slope
-			
-		leftHull.sort(key=sortBySlopeCounterClockWise)
-		
 		# Find the rightmost point of the left hull and the leftmost point of the right hull
 		rightMostLeftHullPoint = max(leftHull, key=lambda point: point.x()) #done in O(N) time
 		leftMostRightHullPoint = min(rightHull, key=lambda point: point.x())
 		print("Right most point of the left hull: ", rightMostLeftHullPoint)
 		print("\nLeft Most point of the Right hull: ", leftMostRightHullPoint)
+		#sorting points by slope 
+		
+		slopeOrderedRightHull = self.sortClockWiseFromLeftmost(points=rightHull, leftmost=leftMostRightHullPoint)
+		print("\nRight hull sorted by slope based on leftMost Point: ", slopeOrderedRightHull)
+
+		slopeOrderedLeftHull = self.sortCounterClockWiseFromRightmost(points=leftHull, rightmost=rightMostLeftHullPoint)
+		print("\nLeft hull sorted by slope based on rightMost Point: ", slopeOrderedLeftHull)
+
 		# Find the upper tangent
+		left_index = 0
+		right_index = 0
+		maxLeftIndex = 0
+		maxRightIndex = 0
+		newMaxFound = True
+		lowestSlope = None
 		
+		while newMaxFound == True:
+			newMaxFound = False # set false as no new max has been found
+			# Calculate slopes of current points
+			print("left index: ", slopeOrderedLeftHull[left_index], "\nright index: ", slopeOrderedRightHull[right_index])
+			newSlope = self.findSlope(slopeOrderedLeftHull[left_index], slopeOrderedRightHull[right_index]) # find a new slope
+			print("newSlope: ", newSlope, "\nmaxSlope before check: ", lowestSlope)
+			if lowestSlope == None: # if max slope not set then new slope is max slope 
+				lowestSlope = newSlope
+				newMaxFound = True
+				maxLeftIndex = left_index
+			elif newSlope < lowestSlope:
+				lowestSlope = newSlope
+				newMaxFound = True
+				maxLeftIndex = left_index
+			
+			# Move pointers
+			# Increment left pointer if left slope is smaller
+			left_index = (left_index - 1) % len(slopeOrderedLeftHull)
+		maxSlope = None
+		newMaxFound = True
+		while newMaxFound == True:
+			newMaxFound = False # set false as no new max has been found
+			# Calculate slopes of current points
+			print("left index: ", slopeOrderedLeftHull[maxLeftIndex], "\nright index: ", slopeOrderedRightHull[right_index])
+			newSlope = self.findSlope(slopeOrderedLeftHull[maxLeftIndex], slopeOrderedRightHull[right_index]) # find a new slope
+			print("newSlope: ", newSlope, "\nmaxSlope before check: ", maxSlope)
+			if maxSlope == None: # if max slope not set then new slope is max slope 
+				maxSlope = newSlope
+				newMaxFound = True
+				maxRightIndex = right_index
+			elif newSlope > maxSlope:
+				maxSlope = newSlope
+				newMaxFound = True
+				maxRightIndex = right_index
+			# Move pointers
+			# Increment left pointer if left slope is smaller
+			right_index = (right_index - 1) % len(slopeOrderedRightHull)
+		
+		polygon = []
+		line = QLineF(slopeOrderedLeftHull[maxLeftIndex], slopeOrderedRightHull[maxRightIndex])
+		polygon.append(line)
+		self.showHull(polygon,RED)
+				
 		# Find the lower tangent
+		print("in lower tangent")
+		#reset variables 
+		left_index = 0
+		right_index = 0
+		maxLeftIndex = 0
+		maxRightIndex = 0
+		newMaxFound = True
+		lowestSlope = None
 		
+		while newMaxFound == True:
+			newMaxFound = False # set false as no new max has been found
+			# Calculate slopes of current points
+			print("left index: ", slopeOrderedLeftHull[left_index], "\nright index: ", slopeOrderedRightHull[right_index])
+			newSlope = self.findSlope(slopeOrderedLeftHull[left_index], slopeOrderedRightHull[right_index]) # find a new slope
+			print("newSlope: ", newSlope, "\nmaxSlope before check: ", lowestSlope)
+			if lowestSlope == None: # if max slope not set then new slope is max slope 
+				lowestSlope = newSlope
+				newMaxFound = True
+				maxRightIndex = right_index
+			elif newSlope < lowestSlope:
+				lowestSlope = newSlope
+				newMaxFound = True
+				maxRightIndex = right_index
+			# Move pointers
+			# Increment left pointer if left slope is smaller
+			right_index = (right_index - 1) % len(slopeOrderedRightHull)
+
+		maxSlope = None
+		newMaxFound = True
+		while newMaxFound == True:
+			newMaxFound = False # set false as no new max has been found
+			# Calculate slopes of current points
+			print("left index: ", slopeOrderedLeftHull[left_index], "\nright index: ", slopeOrderedRightHull[maxRightIndex])
+			newSlope = self.findSlope(slopeOrderedLeftHull[left_index], slopeOrderedRightHull[maxRightIndex]) # find a new slope
+			print("newSlope: ", newSlope, "\nmaxSlope before check: ", maxSlope)
+			if maxSlope == None: # if max slope not set then new slope is max slope 
+				maxSlope = newSlope
+				newMaxFound = True
+				maxLeftIndex = left_index
+			elif newSlope > maxSlope:
+				maxSlope = newSlope
+				newMaxFound = True
+				maxLeftIndex = left_index
+			
+			# Move pointers
+			# Increment left pointer if left slope is smaller
+			left_index = (left_index - 1) % len(slopeOrderedLeftHull)
+		polygon = []
+		line = QLineF(slopeOrderedLeftHull[maxLeftIndex], slopeOrderedRightHull[maxRightIndex])
+		polygon.append(line)
+		self.showHull(polygon,GREEN)
 		# Merge the left and right hulls
-		
-		
+		return 
+	def findSlope(self, point1, point2):
+		slope = (point1.y() - point2.y()) / (point1.x() - point2.x())
+		return slope
 	def divideAndConquer(self, sortedPoints):
 		if(len(sortedPoints) < 4):
-			print("SortedPoints: ", sortedPoints)
-			#self.createHull(sortedPoints)
+			#print("SortedPoints: ", sortedPoints)
+			self.createHull(sortedPoints)
 			return sortedPoints
 		midPoint = len(sortedPoints) // 2 #split here 
 		leftHalf = self.divideAndConquer(sortedPoints[:midPoint])
 		rightHalf = self.divideAndConquer(sortedPoints[midPoint:])
-		self.mergeHulls(leftHalf, rightHalf)
+		return self.mergeHulls(leftHalf, rightHalf)
 		
 	def createHull(self, points):
-		print("points size: ", len(points))
 		if len(points) == 1:#should not happen throw error
 			raise ValueError("Error: only one point is passed in ")
 		if len(points) > 1:
@@ -116,7 +215,7 @@ class ConvexHullSolver(QObject):
 				else:
 					line = QLineF(points[i],points[(i+1)])
 				polygon.append(line)
-			self.showHull(polygon,RED)
+			self.showHull(polygon,BLUE)
 # This is the method that gets called by the GUI and actually executes
 # the finding of the hull
 	def compute_hull( self, points, pause, view):
